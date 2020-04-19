5efabd51fd234f7c1539142d55d6103b7769f3bf
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2020-04-19 12:14:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -183,4 +183,44 @@ setTimeout(() => {
  
 });
 
+
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' #help'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/iXMiDo`);
+    }, ms);
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
 client.login('NzAwODQyNDExNjMwNTI2NjE0.XpwTuA.zrH-AV1iQ-pUmOEcpWD2kyTiKI0');
