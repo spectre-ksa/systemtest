90d270ddca6f03fbefbc1756ae3e7c8c6a25d2d8
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2020-04-20 11:03:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,151 +33,294 @@ if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply('** Yo
              
 });
 
-client.on("message", message => {
-   
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
- if (!args[1]) {
-                                let x5bz1 = new Discord.RichEmbed()
-                                .setDescription("#clear <number>")
-                                .setColor("#0000FF")
-                                message.channel.sendEmbed(x5bz1);
-                            } else {
-                            let messagecount = parseInt(args[1]);
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                                                          message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
-                            let x5bz2 = new Discord.RichEmbed()
-                                                            .setColor("#008000")
-                                .setDescription(":white_check_mark: | Delete " + args[1] + " Message!")
-                                                                                        message.delete("..");
-                                message.channel.sendEmbed(x5bz2);
-                            }
-                          }
-});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///////////////////////////// طط//////////////// قسم الرومات
+
+const pics = JSON.parse(fs.readFileSync('./pics.json' , 'utf8'));
+ client.on('message', message => {
+         if (!message.channel.guild) return;
+
+  let room = message.content.split(" ").slice(1);
+  let findroom = message.guild.channels.find('name', `${room}`)
+  if(message.content.startsWith(prefix + "setMedia")) {
+      if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
+      if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+      if(!room) return message.channel.send('Please Type The Channel Name')
+      if(!findroom) return message.channel.send('Cant Find This Channel')
+      let embed = new Discord.RichEmbed()
+      .setTitle('**Done The MediaOnly Code Has Been Setup**')
+      .addField('Channel:', `${room}`)
+      .addField('Requested By', `${message.author}`)
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(`${client.user.username}`)
+      message.channel.sendEmbed(embed)
+      pics[message.guild.id] = {
+      channel: room,
+      onoff: 'On'
+      },
+      fs.writeFile("./pics.json", JSON.stringify(pics), (err) => {
+      if (err) console.error(err)
+      
+      })
+    }})
+       client.on('message', message => {
+  
+  if(message.content.startsWith(prefix + "toggleMedia")) {
+          if (!message.channel.guild) return;
+
+      if(!message.channel.guild) return  msg.channel.send('**This Command Only For Servers**');
+      if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
+      if(!pics[message.guild.id]) pics[message.guild.id] = {
+        onoff: 'Off'
+      }
+        if(pics[message.guild.id].onoff === 'Off') return [message.channel.send(`**The MediaCode Is __𝐎𝐍__ !**`), pics[message.guild.id].onoff = 'On']
+        if(pics[message.guild.id].onoff === 'On') return [message.channel.send(`**The MediaCode Is __𝐎𝐅𝐅__ !**`), pics[message.guild.id].onoff = 'Off']
+        fs.writeFile("./pics.json", JSON.stringify(pics), (err) => {
+          if (err) console.error(err)
+          
+          })
+        }
+        
+      })
 
 client.on('message', message => {
-if (message.author.bot) return;
-  let args = message.content.split(" ");
-      // By Alpha Codes - AboKhalil 26/7/2019
-  let command = args[0];
- 
-  let user = message.mentions.users.first();
-   
-  let reasonkick = message.content.split(" ").slice(2).join(" ");
- 
-  if (command == prefix + "kick") {
- 
-    if(!message.channel.guild){
-    message.channel.send("**لا يمكن استعمال هذا الأمر في الخاص**");
+                       if (!message.channel.guild) return;
+  if(message.author.bot) return;
+  
+        if(!pics[message.guild.id]) pics[message.guild.id] = {
+        onoff: 'Off'
+      }
+        if(pics[message.guild.id].onoff === 'Off') return;
+
+  if(message.channel.name !== `${pics[message.guild.id].channel}`) return;
+
+   let types = [
+    'jpg',
+    'jpeg',
+    'png',
+    'http://prntscr.com/'
+  ]
+   if (message.attachments.size <= 0) {
+    message.delete();
+    message.channel.send(`${message.author}, This Channel For Media 🖼️ Only !`) 
+    .then(msg => {
+      setTimeout(() => {
+        msg.delete();
+      }, 5000)
+  })
+  return;
 }
-    if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) {
-        message.channel.send("**يجب ان يكون لديك خاصية `KICK_MEMBERS`**");
+   if(message.attachments.size >= 1) {
+    let filename = message.attachments.first().filename
+    console.log(filename);
+    if(!types.some( type => filename.endsWith(type) )) {
+      message.delete();
+      message.channel.send(`${message.author}, This Channel For Media 🖼️ Only !`)
+      .then(msg => {
+        setTimeout(() => {
+          msg.delete();
+        }, 5000)
+      })
+      .catch(err => {
+        console.error(err);
+    });
     }
-    if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) {
-        message.channel.send("**البوت لا يمتلك خاصية `KICK_MEMBERS`**");
-    }
-    if (!user) {
-        message.channel.send("**يجب عليك إختيار الشخص المراد طرده**");
-    }else if (!reasonkick) {
-        message.channel.send("**يجب عليك إدراج سبب الحظر**");
-    } else if (message.guild.member(user).hasPermission("KICK_MEMBERS")){
-    message.channel.send("**لا يمكن طرد هذا الشخص , فهو من الإدارة**");
-    } else {
- 
- 
-    message.guild.member(user).kick()
-    message.channel.send("**The Member Was Kicked **" + user.tag + " **By** : " + message.author.tag);
-    message.channel.send("**Reason : __" + reasonkick + "__**");
-   
-    user.send("**You are Kicked By** : " + message.author.tag);
-    user.send("**Reason : __" + reasonkick + "__**");
-    }
- }
- 
-});
-
-
+  }
+ })
 client.on('message', message => {
-if (message.author.bot) return;
-  let args = message.content.split(" ");
- 
-  let command = args[0];
- 
-  let user = message.mentions.users.first();
- 
-  let bantime = args[2];
-   
-  let reasonban = message.content.split(" ").slice(3).join(" ");
-   
-  let timefilter;
- 
-  if (command == prefix + "ban") {
-     
-    if(!message.channel.guild){
-    message.channel.send("**لا يمكن استعمال هذا الأمر في الخاص**");
-}
-    if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) {
-        message.channel.send("**يجب ان يكون لديك خاصية `BAN_MEMBERS`**");
+  if(message.content.startsWith(prefix + "infoMedia")) {
+let embed = new Discord.RichEmbed()
+.addField('Channel Status', `${pics[message.guild.id].onoff}`)
+.addField('Media Channel', `${pics[message.guild.id].channel}`)
+.addField('Requested By', `${message.author}`)
+.setThumbnail(message.author.avatarURL)
+.setFooter(`${client.user.username}`)
+message.channel.sendEmbed(embed)
+  }})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// قسم الالعاب 
+client.on('message', message => {
+    if (message.content == `${prefix}جمع`) {
+        var x = ["212+212=?",
+"321+43=?",
+"4534+23",
+"23+3434=?",
+"2311+32=?",
+"765+343=?",
+"343+1121=?",
+"43234+1=?",
+"10000000000+2=?",
+"232+21=?",
+"12+23=?",                 
+];
+        var x2 = ['424',
+        "364",
+        "4557",
+        "3457",
+		"2343",
+		"1108",
+    "1464",
+    "43235",
+   "10000000002",
+  "253",
+  "35",
+                  
+        ];
+        
+        var x3 = Math.floor(Math.random()*x.length)
+        message.channel.send(` اول شخص يحل جمع : __**${x[x3]}**_
+لديك 15 ثانية للاجابة`).then(msg1=> {
+            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 15000,
+                errors : ['time']
+            })
+        r.catch(() => {
+            return message.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح 
+            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+        })
+        
+        r.then((collected)=> {
+            message.channel.send(`${collected.first().author} لقد قمت بحل جمع في الوقت المناسب  `);
+        })
+        })
     }
-    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) {
-        message.channel.send("**البوت لا يمتلك خاصية `BAN_MEMBERS`**");
-    }
-   
-    if (!user){
-        message.channel.send("**يجب عليك إختيار الشخص المراد حظره**");
-    }else if (!bantime){
-        message.channel.send(`**
-        يجب عليك اختيار وقت الحظر
-        15m حظر ربع ساعه
-        30m حظر نصف ساعه
-        1h حظر ساعه
-        3h حظر ثلاث ساعات
-        1d حظر يوم كامل
-        3d حظر ثلاث ايام
-        1w حظر اسبوع
-        1mon حظر شهر كامل
-        **`);
-    }else if (!reasonban){
-        message.channel.send("**يجب عليك إدراج سبب الحظر**");
-    }
-    if (message.guild.member(user).hasPermission("BAN_MEMBERS")){
-    message.channel.send("**لا يمكن طرد هذا الشخص , فهو من الإدارة**");
-    } else {
-// By Alpha Codes - AboKhalil 26/7/2019
-    if (bantime = "15m") {
-    timefilter = 150000;
-    } else if (bantime = "30m") {
-        timefilter = 300000;
-    } else if (bantime = "1h") {
-        timefilter = 600000;
-    } else if (bantime = "3h") {
-        timefilter = 1800000;
-    } else if (bantime = "1d") {
-        timefilter = 14400000;
-    } else if (bantime = "3d") {
-        timefilter = 43200000;
-    } else if (bantime = "1w") {
-        timefilter = 100800000;
-    } else if (bantime = "1mon"){
-        timefilter = 432000000;
-    }
-    message.guild.member(user).ban()
-    message.channel.send("**The Member Was Banned **" + user.tag + " **By** : " + message.author.tag);
-    message.channel.send("**Reason : __" + reasonban + "__**");
-   
-    user.send("**You are Banned By** : " + message.author.tag);
-    user.send("**Reason : __" + reasonban + "__**");
-setTimeout(() => {
- 
-  message.guild.unban(bannedman);
- 
-        }, timefilter);
-    }
- }
- 
-});
+})
+
+const kingmas = [
+    '*** منشن الجميع وقل انا اكرهكم. ***',
+ '*** اتصل على امك و قول لها انك تحبها :heart:. ***',
+    '***     تصل على الوالده  و تقول لها  احب وحده.***',
+    '*** تتصل على شرطي تقول له عندكم مطافي.***',
+    '*** صور اي شيء يطلبه منك الاعبين.***',
+    '*** اكتب في الشات اي شيء يطلبه منك الاعبين في الخاص. ***',
+    '*** اتصل على احد من اخوياك  خوياتك , و اطلب منهم مبلغ على اساس انك صدمت بسيارتك.***',
+    '*** اعطي اي احد جنبك كف اذا مافيه احد جنبك اعطي نفسك و نبي نسمع صوت الكف.***',
+    '***  تروح عند شخص تقول له احبك. ***',
+    '***روح عند اي احد بالخاص و قول له انك تحبه و الخ.***',
+    '*** اذهب الى واحد ماتعرفه وقل له انا كيوت وابي بوسه. ***',
+    '*** روح الى اي قروب عندك في الواتس اب و اكتب اي شيء يطلبه منك الاعبين  الحد الاقصى 3 رسائل. ***',
+    '*** اذا انت ولد اكسر اغلى او احسن عطور عندك اذا انتي بنت اكسري الروج حقك او الميك اب حقك. ***',
+    '*** ذي المرة لك لا تعيدها.***',
+    '*** ارمي جوالك على الارض بقوة و اذا انكسر صور الجوال و ارسله في الشات العام.***',
+    '*** اتصل على ابوك و قول له انك رحت مع بنت و احين هي حامل..... ***',
+    '*** تكلم باللهجة السودانية الين يجي دورك مرة ثانية.***',
+    '***سو مشهد تمثيلي عن مصرية بتولد.***',
+    '*** قول نكتة اذا و لازم احد الاعبين يضحك اذا محد ضحك يعطونك ميوت الى ان يجي دورك مرة ثانية. ***',
+    '*** قول نكتة اذا و لازم احد الاعبين يضحك اذا محد ضحك يعطونك ميوت الى ان يجي دورك مرة ثانية.***',
+    '*** سامحتك خلاص مافيه عقاب لك :slight_smile:. ***',
+    '*** اذهب الى واحد ماتعرفه وقل له انا كيوت وابي بوسه.***',
+    '*** تتصل على الوالده  و تقول لها خطفت شخص. ***',
+    '*** روح اكل ملح + ليمون اذا مافيه اكل اي شيء من اختيار الي معك.  ***'
+ ]
+  client.on('message', message => {
+    var prefix = `${prefix}`;
+  if (message.content.startsWith(`${prefix}` + 'حكم')) {
+   var mariam= new Discord.RichEmbed()
+   .setTitle("لعبة حكم ..")
+   .setColor('RANDOM')
+   .setDescription(`${kingmas[Math.floor(Math.random() * kingmas.length)]}`)
+    message.channel.sendEmbed(mariam);
+    message.react(":thinking:")
+   }
+ });
+
+client.on('message', PuP => {
+  let args = PuP.content.split(" ").slice(1).join(" ")
+  if (PuP.content.startsWith(`${prefix}sr`)) {
+                if (!PuP.member.hasPermission("MANAGE_SERVER"))  return;
+                if(!args) return PuP.channel.send('`**يرجي ادخال اسم السرفر الجديد**`');
+                PuP.guild.owner.send(`**ى تغيير اسم السرفر الي ${args}
+                بواسطة : <@${PuP.author.id}>**`)
+                PuP.guild.setName(args)
+                PuP.channel.send(`**تم تغير اسم السيرفر الي : __${args}__ **`);
+                
+       }
+
+       });
+
+
+
+
+
+
+
+
+
 
 
 client.on('ready', function(){
@@ -217,6 +360,7 @@ client.on('ready', function(){
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+    
 });
 
 client.login(process.env.BOT_TOKEN);
